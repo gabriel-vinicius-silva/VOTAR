0d1fc7f49ebd63ea8be79d3ce06a83c8d14a7425
unknown <gabrieuvinici@gmail.com>
2023-11-27 19:30:40 -0300

    chove la fora

--- sketch.js
+++ sketch.js
@@ -6,7 +6,16 @@ function votarBolsonaro(){
     voteBolsonaro = voteBolsonaro +1;
     alert("voce votou em bolsonaro");
 }
+function votarLula(){
+    voteLula = voteLula +1;
+
+    alert("voce votou em Lula");
+}
+function votarSimone(){
+    voteBolsonaro = voteBolsonaro +1;
+    alert("voce votou em bolsonaro");
+}
 
 function mostraVotos(){
-   
+    alert( "voto bolsonaro : " + voteBolsonaro + "\n"+"votos lula : "+ voteLula+ "\n" + "votos Simone : " + voteSimone);
 }
