539f2cfe74e786acff64c9fd1174a4f2dc7663bd
unknown <gabrieuvinici@gmail.com>
2023-11-27 19:18:36 -0300

    contanto votos

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var voteBolsonaro = 0;
+var voteLula = 0 ; 
+var voteSimone = 0 ; 
+
+function votarBolsonaro(){
+    voteBolsonaro = voteBolsonaro +1;
+    alert("voce votou em bolsonaro");
+}
+
+function mostraVotos(){
+   
+}
